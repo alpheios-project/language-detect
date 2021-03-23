60a83cdfeabd0d2dad0dc7f48d8e59299a0bb399
Irina <irina060981@yandex.ru>
2021-03-23 20:37:01 +1000

    rename class name and remove default

--- tests/language-detect.test.js
+++ tests/language-detect.test.js
@@ -1,9 +1,9 @@
 /* eslint-env jest */
 /* eslint-disable no-unused-vars */
 
-import DetectLanguage from '@/detect-language.js'
+import LanguageDetect from '@/language-detect.js'
 
-describe('detect-language.test.js', () => {
+describe('language-detect.test.js', () => {
   console.error = function () {}
   console.log = function () {}
   console.warn = function () {}
@@ -20,43 +20,43 @@ describe('detect-language.test.js', () => {
     jest.clearAllMocks()
   })
 
-  it('1 DetectLanguage - check greek', () => {
+  it('1 LanguageDetect - check greek', () => {
     const testStr = 'τά ταῖν ταῖν τοῦ εγώ εμένα ἐμαυτοῦ ταύταις ένα δύο τρία όμορφος νέος γλυκός συνεχής πλατύς οξύς ἄνδρα μοι ἔννεπε, μοῦσα, πολύτροπον πρόσφυμα ξηρή σκληρή ζώνη ινώδους συνδετικού ιστού που συνδέει συνήθως μύες με οστά ἥρωϊ Δαναὸς ἀφίκτωρ λεπτοψαμάθων'
-    expect(DetectLanguage.detect(testStr)).toEqual('grc')
+    expect(LanguageDetect.detect(testStr)).toEqual('grc')
   })
 
-  it('2 DetectLanguage - check Persian', () => {
+  it('2 LanguageDetect - check Persian', () => {
     const testStr = 'بادائیدنبه نام خداوند جان و خرد کزین برتر اندیشه برنگذر'
-    expect(DetectLanguage.detect(testStr)).toEqual('per')
+    expect(LanguageDetect.detect(testStr)).toEqual('per')
   })
 
-  it('3 DetectLanguage - check arabic', () => {
+  it('3 LanguageDetect - check arabic', () => {
     const testStr = 'اَلدٌّنيَا دَارُ مَمَرٍ لاَ دَارُ مَقَرٍ * سُلطَان بِلاَ عَدلٍ كَنَهرٍ'
-    expect(DetectLanguage.detect(testStr)).toEqual('ara')
+    expect(LanguageDetect.detect(testStr)).toEqual('ara')
   })
 
-  it('4 DetectLanguage - check latin', () => {
+  it('4 LanguageDetect - check latin', () => {
     const testStr = 'fero ferre tuli latus ferant'
-    expect(DetectLanguage.detect(testStr)).toEqual('lat')
+    expect(LanguageDetect.detect(testStr)).toEqual('lat')
   })
 
-  it('5 DetectLanguage - check geez', () => {
+  it('5 LanguageDetect - check geez', () => {
     const testStr = 'ይትባረክ፡ እግዚአብሔር፡ አምላከ፡ እስራኤል፡ አምላክ፡ ለኵሉ፡ መንፈስ፡ ወለኵሉ፡ ዘሥጋ፡ ወይቤሎ፡ እግዚአብሔር፡ ለእዝራ፡ አእምር፡ ወጠይቅ፡'
-    expect(DetectLanguage.detect(testStr)).toEqual('gez')
+    expect(LanguageDetect.detect(testStr)).toEqual('gez')
   })
 
-  it('6 DetectLanguage - check chinese', () => {
+  it('6 LanguageDetect - check chinese', () => {
     const testStr = '天地玄黃，宇宙洪荒。日月盈昃，辰宿列張。寒來暑往，秋收冬藏。閏余成歲，律呂調陽。子曰學'
-    expect(DetectLanguage.detect(testStr)).toEqual('zho')
+    expect(LanguageDetect.detect(testStr)).toEqual('zho')
   })
 
-  it('7 DetectLanguage - check syriac', () => {
+  it('7 LanguageDetect - check syriac', () => {
     const testStr = 'ܥܲܠ ܟܵܘܟ݁ܒ̣ܵܐ ܗܵܘ̇ ܕܐܸܬܼܚܙܝܼ ܠܲܡܓ̣ܘܼ̈ܫܹܐ'
-    expect(DetectLanguage.detect(testStr)).toEqual('syr')
+    expect(LanguageDetect.detect(testStr)).toEqual('syr')
   })
   
-  it('7 DetectLanguage - check default', () => {
-    const testStr ='texto de prueba'
-    expect(DetectLanguage.detect(testStr)).toEqual('lat')
+  it('7 LanguageDetect - check out of list', () => {
+    const testStr ='Тестовый текст'
+    expect(LanguageDetect.detect(testStr)).not.toBeDefined()
   })
 })
