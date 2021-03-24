a8bec8cf7d24d322ff6732e69e1b5b5fb4a2aa86
Irina <irina060981@yandex.ru>
2021-03-24 10:39:27 +1000

    update path

--- dist/alpheios-language-detect.js
+++ dist/alpheios-language-detect.js
@@ -112,7 +112,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "LanguageDetect": () => (/* reexport safe */ _language_detect_js__WEBPACK_IMPORTED_MODULE_0__.default)
 /* harmony export */ });
-/* harmony import */ var _language_detect_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @/language-detect.js */ "./language-detect.js");
+/* harmony import */ var _language_detect_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./language-detect.js */ "./language-detect.js");
 
 
 
